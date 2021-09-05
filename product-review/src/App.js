1fe951cfd381255e3e25f79c5dce52acbade81c9
imupallnighttogitsome <artishard.palmer@gmail.com>
2021-09-04 23:09:02 -0500

    Code Challenge Complete

--- product-review/src/App.js
+++ product-review/src/App.js
@@ -7,13 +7,12 @@ import Modal from './components/reviews/Modal';
 
 function App() {
   const [show, setShow] = useState(false);
-  const [enlarge, setEnlarge] = useState(false);
   
   return (
     <div className="App">
       <Modal onClose={ () => setShow(false) } show={show}/>
       <UserInfo onOpen={ () => setShow(true) } show={show} setShow={setShow}/>
-      <ReviewPhotos onEnlarge={ () => setEnlarge(true) } enlarge={enlarge}/>
+      <ReviewPhotos/>
     </div>
   );
 }
